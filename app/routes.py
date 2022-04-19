1cda82331cadc47e1814c8f5e7b1e144d7c56247
Khoi <kdng96@gmail.com>
2022-04-19 15:26:37 -0700

    displayed name and dates

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,25 @@
 from flask import Blueprint, render_template
+import os
+import psycopg2
 
 
 bp = Blueprint('main', __name__, url_prefix="/")
 
+CONNECTION_PARAMETERS = {
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASS"),
+    "dbname": os.environ.get("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+}
 
 @bp.route("/")
 def main():
-    return render_template("main.html") #looks for "template directory"
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as curs:
+            curs.execute("""
+                SELECT id, name, start_datetime, end_datetime
+                FROM appointments
+                ORDER BY start_datetime;
+            """)
+            results = curs.fetchall()
+    return render_template("main.html", rows=results) #looks for "template directory"
